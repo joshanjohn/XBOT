d5285cc8a54dde4cb270bb457ad7136633407ac5
Joshanjohn <54502617+joshanjohn@users.noreply.github.com>
2024-03-29 17:17:59 +0000

    Added API Key and BASE URL in WeatherApiConnection
    Added constant API Key and BASE URL in WeatherApiConnection class

--- XBOT/src/main/java/weather/WeatherApiConnection.java
+++ XBOT/src/main/java/weather/WeatherApiConnection.java
@@ -7,12 +7,18 @@ public class WeatherApiConnection {
 
     private String cityName; // Name of the city
 
+    // API KEY for weather
+    private static final String API_KEY = "dc63076a45354945b01205300241503";
+
+    // API URL without key and parameters
+    private static final String BASE_URL = "https://api.weatherapi.com/v1/forecast.json?key=";
+
     private HttpURLConnection connect() throws Exception {
         /*
          * method to establish connection with weather API it take cityName as Argument
          * and connect with API. method return an HttpURLConnection object
          */
-        
+
         return null;
     }
 
