4f88d48013a866e36956bda576ab4737f88c6f02
Joshanjohn <54502617+joshanjohn@users.noreply.github.com>
2024-03-29 15:44:37 +0000

    Added days and cityname attribute to WeatherApiConnection class
    Added days and city name attribute to WeatherApiConnection.java class

--- XBOT/src/main/java/weather/WeatherApiConnection.java
+++ XBOT/src/main/java/weather/WeatherApiConnection.java
@@ -3,12 +3,16 @@ package weather;
 import java.net.HttpURLConnection;
 
 public class WeatherApiConnection {
+    private int days; // number of days
+
+    private String cityName; // Name of the city
+
     private HttpURLConnection connect() throws Exception {
         /*
-		 * method to establish connection with weather API it take cityName as Argument
-		 * and connect with API. method return an HttpURLConnection object
-		 */
-
+         * method to establish connection with weather API it take cityName as Argument
+         * and connect with API. method return an HttpURLConnection object
+         */
+        
         return null;
     }
 
