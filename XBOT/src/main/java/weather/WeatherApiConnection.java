f5a24c36469dd103a88e80bb40ae8a818e85a456
Joshanjohn <54502617+joshanjohn@users.noreply.github.com>
2024-03-29 15:29:13 +0000

    Added connect method in  WeatherApiConnection class
    Added connect method in  WeatherApiConnection.java  class to establish a connection with weather API it takes cityName as an Argument and connects with API. method returns an HttpURLConnection object.

--- XBOT/src/main/java/weather/WeatherApiConnection.java
+++ XBOT/src/main/java/weather/WeatherApiConnection.java
@@ -1,5 +1,15 @@
 package weather;
 
+import java.net.HttpURLConnection;
+
 public class WeatherApiConnection {
+    private HttpURLConnection connect() throws Exception {
+        /*
+		 * method to establish connection with weather API it take cityName as Argument
+		 * and connect with API. method return an HttpURLConnection object
+		 */
+
+        return null;
+    }
 
 }
