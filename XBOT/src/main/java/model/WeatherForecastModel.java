3c5255c8a859a306d689538a07af05c5dd14efad
Joshanjohn <54502617+joshanjohn@users.noreply.github.com>
2024-03-30 16:28:40 +0000

    Added WeatherForecastModel constructor in  WeatherForecastModel class
    Added WeatherForecastModel constructor with forecastData attribute in WeatherForecastModel.java class

--- XBOT/src/main/java/model/WeatherForecastModel.java
+++ XBOT/src/main/java/model/WeatherForecastModel.java
@@ -1,5 +1,15 @@
 package model;
 
-public class WeatherForecastModel {
+import org.json.JSONObject;
 
+import org.json.JSONObject;
+
+public class WeatherForecastModel {
+	// attribute for forecastData
+	private final JSONObject forecastData;
+
+	// Constructor for WeatherForecastModel
+	public WeatherForecastModel(String forecastJson) {
+		this.forecastData = new JSONObject(forecastJson);
+	}
 }
