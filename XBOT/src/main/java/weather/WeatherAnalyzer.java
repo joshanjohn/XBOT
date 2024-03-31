7060d8a4f7c5adf4cd59a3a6c956440796e466fa
Joshanjohn <54502617+joshanjohn@users.noreply.github.com>
2024-03-31 18:36:55 +0100

    Added stub getWeatherForecastModel method in WeatherAnalyzer class
    Added stub getWeatherForecastModel() method in WeatherAnalyzer.java class

--- XBOT/src/main/java/weather/WeatherAnalyzer.java
+++ XBOT/src/main/java/weather/WeatherAnalyzer.java
@@ -1,5 +1,12 @@
 package weather;
 
+import model.WeatherForecastModel;
+
+/*
+ * Use of this class is :
+ * this class will help to convert the weather data into model objects (WeatherForecastModel)
+ */
+
 public class WeatherAnalyzer {
 	private final WeatherApiConnection weatherApiConnection;
 
@@ -9,4 +16,10 @@ public class WeatherAnalyzer {
 		this.weatherApiConnection = new WeatherApiConnection(cityName, days);
 	}
 
+	public WeatherForecastModel getWeatherForecastModel() throws Exception {
+		/*
+		 * Method to return the weather forecast model
+		 */
+		return null;
+	}
 }
