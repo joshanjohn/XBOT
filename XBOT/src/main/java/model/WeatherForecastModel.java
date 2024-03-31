c6ba20fbaead6ebf4ee107425b80927c2a72171a
HARSH <160406269+7-FLASH-7@users.noreply.github.com>
2024-03-31 12:34:04 +0100

    constructor for Weather forecast mode;
    the constructor takes one parameter which is a string forecast Json

--- XBOT/src/main/java/model/WeatherForecastModel.java
+++ XBOT/src/main/java/model/WeatherForecastModel.java
@@ -3,6 +3,11 @@ package model;
 import org.json.JSONObject;
 
 public class WeatherForecastModel{
-    private final JSONObject forecastData = new JSONObject();
+    private final JSONObject forecastData ;
+
+    // Constructor for WeatherForecastModel
+	public WeatherForecastModel(String forecastJson) {
+		this.forecastData = new JSONObject(forecastJson);
+	}
 
 }
