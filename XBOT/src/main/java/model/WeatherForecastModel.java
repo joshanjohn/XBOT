194d49509495d6f52ff93a54bb49657bd260d9ad
anna <2004gromyko@gmail.com>
2024-03-31 16:30:43 +0100

    Added getForecastDay method in WeatherForecastModel

--- XBOT/src/main/java/model/WeatherForecastModel.java
+++ XBOT/src/main/java/model/WeatherForecastModel.java
@@ -1,5 +1,6 @@
 package model;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 public class WeatherForecastModel{
@@ -8,6 +9,14 @@ public class WeatherForecastModel{
     // Constructor for WeatherForecastModel
 	public WeatherForecastModel(String forecastJson) {
 		this.forecastData = new JSONObject(forecastJson);
+	}
+
+	public ForecastDayModel getForecastDay(int index) {
+		/*
+		 * Method to get the forecast data for a specific day. The index use an index
+		 * value to specify the day
+		 */
+		return null;
 	}
 
 }
