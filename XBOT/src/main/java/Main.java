e8ff47b3ca3ce59f0e3f974207942550dd45e25c
anna <2004gromyko@gmail.com>
2024-04-01 19:15:02 +0100

    Created greeting in Main

--- XBOT/src/main/java/Main.java
+++ XBOT/src/main/java/Main.java
@@ -1,6 +1,7 @@
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Hello Anna");
+		System.out.println("Hello! My name is XBOT.");
+		System.out.println("Please enter 5 cities.");
 	}
 }
