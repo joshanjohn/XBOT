687e42e5e34ebff6e7e94b0e98f9d827b64afa62
anna <2004gromyko@gmail.com>
2024-04-01 17:34:39 +0100

    Fixed ForecastDayModel class

--- XBOT/src/main/java/model/ForecastDayModel.java
+++ XBOT/src/main/java/model/ForecastDayModel.java
@@ -4,9 +4,9 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 
 public class ForecastDayModel {
-    private final JSONObject forecastDayJson ;
-    
-    // Constructor to initialize the ForecastDayModel with forecast day JSON data
+	private final JSONObject forecastDayJson;
+
+	// Constructor to initialize the ForecastDayModel with forecast day JSON data
 	public ForecastDayModel(JSONObject forecastDayJson) {
 		this.forecastDayJson = forecastDayJson;
 	}
