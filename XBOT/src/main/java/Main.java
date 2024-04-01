1e27e98748b7a88f51161ec92635ce6ad2db191a
anna <2004gromyko@gmail.com>
2024-04-01 19:55:43 +0100

    Created isValidCity method in Validation

--- XBOT/src/main/java/Main.java
+++ XBOT/src/main/java/Main.java
@@ -1,7 +1,20 @@
+import java.util.Scanner;
+
+import Validation.Validation;
 
 public class Main {
+	public static Validation validation = new Validation();
+	public static Scanner sc = new Scanner(System.in);
 	public static void main(String[] args) {
 		System.out.println("Hello! My name is XBOT.");
 		System.out.println("Please enter 5 cities.");
+		String[] cities = new String[5];
+		for(int i = 1; i != 6; i++ )
+		{
+			System.out.println("City " + i + ": ");
+			String city = sc.nextLine();
+			if(validation.isValidCity(city)) cities[i - 1] = city; 
+		}
 	}
+
 }
--- XBOT/src/main/java/Validation/Validation.java
+++ XBOT/src/main/java/Validation/Validation.java
@@ -1,5 +1,10 @@
 package Validation;
 
 public class Validation {
+
+    public boolean isValidCity(String city)
+    {
+       return false;
+    }
     
 }
