e26be2f6aa68b6df818a57a45231d44851f9e99c
anna <2004gromyko@gmail.com>
2024-04-01 23:10:41 +0100

    Implemented isValidCity method in Validation class

--- XBOT/src/main/java/Validation/Validation.java
+++ XBOT/src/main/java/Validation/Validation.java
@@ -1,10 +1,30 @@
 package Validation;
 
+import java.util.regex.Pattern;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+
+
 public class Validation {
+
+    public Scanner sc = new Scanner(System.in);
 
     public boolean isValidCity(String city)
     {
-       return false;
+        //pattern for checking city name
+        String cityNamePattern = "^[A-Za-z]+( [A-Za-z]+)*(-[A-Za-z]+( [A-Za-z]+)*)*$";
+        // flag to stop the loop if the input is valid
+        boolean valid = false;
+
+        while (!valid) {
+            Pattern regex = Pattern.compile(cityNamePattern);
+            Matcher matcher = regex.matcher(city.trim());
+            if (matcher.matches()) 
+                return true;
+            else 
+               return false;
+        }
+        return true;
     }
     
 }
