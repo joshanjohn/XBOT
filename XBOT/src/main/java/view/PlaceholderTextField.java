9db7afd91093129a216637161ba781407fb51781
anna <2004gromyko@gmail.com>
2024-04-16 21:59:53 +0100

    Created constructor in PlaceholderTextField

--- XBOT/src/main/java/view/PlaceholderTextField.java
+++ XBOT/src/main/java/view/PlaceholderTextField.java
@@ -1,5 +1,13 @@
 package view;
 
-public class PlaceholderTextField {
+
+import javax.swing.*;
+import java.awt.*;
+
+public class PlaceholderTextField extends JTextField {
+    public PlaceholderTextField(String placeholder) {
+        setForeground(Color.GRAY); // Set text color to gray
+        setText(placeholder);
+    }
 
 }
