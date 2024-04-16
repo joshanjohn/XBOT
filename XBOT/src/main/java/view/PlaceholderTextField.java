b631a60abf00da0723269ca0a423ddb27e15bb55
anna <2004gromyko@gmail.com>
2024-04-16 22:12:48 +0100

    Created addFocusListener in PlaceholderTextField

--- XBOT/src/main/java/view/PlaceholderTextField.java
+++ XBOT/src/main/java/view/PlaceholderTextField.java
@@ -3,11 +3,24 @@ package view;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
 
 public class PlaceholderTextField extends JTextField {
     public PlaceholderTextField(String placeholder) {
         setForeground(Color.GRAY); // Set text color to gray
         setText(placeholder);
+        addFocusListener(new FocusListener() {
+            @Override
+            public void focusGained(FocusEvent e) {
+                return;
+            }
+
+            @Override
+            public void focusLost(FocusEvent e) {
+                return;
+            }
+        });
     }
 
 }
