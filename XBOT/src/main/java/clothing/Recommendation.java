cca3d90832743057424bf91d77d56a8a5dca9445
HARSH <160406269+7-FLASH-7@users.noreply.github.com>
2024-04-16 12:21:16 +0100

    creating method getRecommendation
    it will recommend your clothes according to the temperature outside for three days

--- XBOT/src/main/java/clothing/Recommendation.java
+++ XBOT/src/main/java/clothing/Recommendation.java
@@ -1,5 +1,15 @@
 package clothing;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import model.ForecastDayModel;
+import model.WeatherForecastModel;
+import weather.WeatherAnalyzer;
+
 public class Recommendation {
 
+	public static String getRecommendation(String cityName) {
+		
+	}
 }
