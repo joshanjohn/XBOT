9439f2a1ce75755900e6d98f70756f9a8f7342c4
Joshanjohn <54502617+joshanjohn@users.noreply.github.com>
2024-04-16 09:49:59 +0100

    declare private attributes in xbot class
    declare private attributes inputField, chatArea, scrollPane in xbot java class

--- XBOT/src/main/java/view/XBOT.java
+++ XBOT/src/main/java/view/XBOT.java
@@ -1,5 +1,11 @@
 package view;
 
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+
 public class XBOT {
+	private PlaceholderTextField inputField; // Declaration of PlaceholderTextField object
+	private JTextArea chatArea; // Declaration of JTextArea object
+	private JScrollPane scrollPane; // Declaration of JScrollPane object
 
 }
