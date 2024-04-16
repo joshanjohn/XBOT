b5fce8a7d0ecdb5d8052a024bebe0e28f28565fd
Joshanjohn <54502617+joshanjohn@users.noreply.github.com>
2024-04-16 16:46:22 +0100

    Implmented weatherSummary method and test successfully
    Implmented weatherSummary method  in  Recommendation java class and test successfully

--- XBOT/src/main/java/clothing/Recommendation.java
+++ XBOT/src/main/java/clothing/Recommendation.java
@@ -40,17 +40,19 @@ public class Recommendation {
 	// }
 
 	/**
-	 * Generates a weather summary based on provided parameters.
-	 *
-	 * @param date      The date for which the weather summary is generated.
-	 * @param temp      The temperature for the given date.
-	 * @param condition The weather condition for the given date.
-	 * @param wind      The wind speed for the given date.
-	 * @return A formatted weather summary string.
-	 */
-	protected static String weatherSummary(String date, String temp, String condition, Double wind) {
-		return null;
-	}
+     * Generates a weather summary based on provided parameters.
+     *
+     * @param date      The date for which the weather summary is generated.
+     * @param temp      The temperature for the given date.
+     * @param condition The weather condition for the given date.
+     * @param wind      The wind speed for the given date.
+     * @return A formatted weather summary string.
+     */
+    protected static String weatherSummary(String date, String temp, String condition, Double wind) {
+        return "\n\t> On " + generateDate(date) + ", the weather with most feel like \n\ttemperature of " + temp
+                + "°C, \n\taccompanied by " + condition + " weather conditions and \n\ta wind speed of " + wind
+                + "km/h.";
+    }
 
 	// function to convert the date String into the desired format
 	private static String generateDate(String dateString) {
