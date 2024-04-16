9083ce69ac7cf6a4f93a715e2cc8ee64fbdd2d4d
HARSH <160406269+7-FLASH-7@users.noreply.github.com>
2024-04-16 12:54:16 +0100

    created the method but still there are some errors
    trying to fix the errors as soon as possible

--- XBOT/src/main/java/clothing/Recommendation.java
+++ XBOT/src/main/java/clothing/Recommendation.java
@@ -10,6 +10,31 @@ import weather.WeatherAnalyzer;
 public class Recommendation {
 
 	public static String getRecommendation(String cityName) {
+		StringBuilder data = new StringBuilder();
+		try {
+			WeatherAnalyzer analyser = new WeatherAnalyzer(cityName, 3);
 		
+			//gets the weather forecast
+			WeatherForecastModel weatherForecastModel = analyzer.getWeatherForecastModel();
+			
+			//loop through the forecast for the next 3 days
+			for(int i = 0; i < 3; i++) {
+				ForecastDayModel forecastDay = weatherForecastModel.getForecastDay(i);
+				String date = forecastDay.getDate();
+				Double temp = forecastDay.getMostFeelsLikeTemperature();
+				String weatherCondition = forecastDay.getWeatherCondition();
+				Double wind = forecast.getWind();
+				if (i==0) {
+					data.append(weatherSummary(date,date, weatherCondition,wind));
+				}else {
+					data.append("\n\n\n" + weatherSummary(date,date, weatherCondition,wind));
+				}
+				data.append("\n\n\tRecommendations for this day"));
+				data.append("\n\t" + clothes(temp,weatherCondition));
+			}return null;
+		}catch(Exception e) {
+		}
+		data.replace(0, data.length(),"No data found"));
+		return null;
 	}
 }
