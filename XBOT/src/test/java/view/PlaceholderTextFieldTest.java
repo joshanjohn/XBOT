fbe225045cc812646ee64e34adc00ce27d488fb5
anna <2004gromyko@gmail.com>
2024-04-16 22:56:23 +0100

    Added testAddFocusListener method in PlaceholderTextFieldTest

--- XBOT/src/test/java/view/PlaceholderTextFieldTest.java
+++ XBOT/src/test/java/view/PlaceholderTextFieldTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import java.awt.event.FocusListener;
 
 class PlaceholderTextFieldTest {
 
@@ -27,8 +28,16 @@ class PlaceholderTextFieldTest {
 	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+	void testAddFocusListener() {
+		PlaceholderTextField textField = new PlaceholderTextField("Enter text here");
 
+		// Get the focus listeners added to the text field
+		FocusListener[] focusListeners = textField.getFocusListeners();
+
+		// Check if there is at least one focus listener
+		assertTrue(focusListeners.length > 0);
+
+		// Check if the first focus listener is an instance of FocusListener
+		assertTrue(focusListeners[0] instanceof FocusListener);
+	}
 }
