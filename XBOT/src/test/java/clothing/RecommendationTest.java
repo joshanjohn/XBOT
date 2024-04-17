2006db8f425ba27cc6873a134a894ee943f0f30a
Joshanjohn <54502617+joshanjohn@users.noreply.github.com>
2024-04-17 16:22:57 +0100

    test and implemented weatherSummary method
    test and implemented weatherSummary method in the Recommendation class

--- XBOT/src/test/java/clothing/RecommendationTest.java
+++ XBOT/src/test/java/clothing/RecommendationTest.java
@@ -27,6 +27,33 @@ class RecommendationTest {
 	}
 
 	@Test
+	public void testWeatherSummary() {
+		// Instantiate the Recommendation class
+		Recommendation recommendation = new Recommendation();
+
+		// Define test input values
+		String date = "2024-04-15";
+		String temp = "20";
+		String condition = "Sunny";
+		Double wind = 10.5;
+
+		// Define expected and unexpected output strings
+		String expected = "\n\t> On April 15, 2024, the weather with most feel like \n\ttemperature of 20°C, \n\taccompanied by Sunny weather conditions and \n\ta wind speed of 10.5km/h.";
+		String unexpected = "\n\t> On March 15, 2024, the weather with most feel like \n\ttemperature of 89°C, \n\taccompanied by Sunny weather conditions and \n\ta wind speed of 10.5km/h.";
+
+		// Assert that the generated weather summary is not null
+		assertNotNull(recommendation.weatherSummary(date, temp, condition, wind));
+
+		// Assert that the generated weather summary matches the expected output
+		assertEquals(expected, recommendation.weatherSummary(date, temp, condition, wind));
+
+		// Assert that the generated weather summary is not equal to the unexpected
+		// output
+		assertNotEquals(unexpected, recommendation.weatherSummary(date, temp, condition, wind));
+
+	}
+
+	@Test
 	public void testGenerateDate() {
 		Recommendation recommendation = new Recommendation();
 
