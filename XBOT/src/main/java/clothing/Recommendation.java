28ba3f2c66b4504d5dde8c79e67dc2f911dd4ed6
anna <2004gromyko@gmail.com>
2024-04-17 01:06:15 +0100

    Implementation of clothes method in Recommendation

--- XBOT/src/main/java/clothing/Recommendation.java
+++ XBOT/src/main/java/clothing/Recommendation.java
@@ -69,7 +69,32 @@ public class Recommendation {
 	}
 
 	protected static String clothes(Double temp, String weatherCondition, Double wind) {
-		return null;
+		//Creating variables
+		int windResult = analyseWind(wind);
+		String recommendation = "";
+
+		//Analysing wind speed
+		if (windResult == 4) {
+			recommendation += "IT IS STORM! Stay at home!";
+			return recommendation;
+		}
+		//Different results according to temperature
+		if (temp > 20)
+			recommendation += "It is hot! Wear something light, like shorts and t-shirt. Don't forget about headdress. ";
+		else if (temp < 20 && temp >= 15)
+			recommendation += "It is warm, but not hot. Wear something light but take the jacket.";
+		else if (temp < 15 && temp >= 10)
+			recommendation += "It is a little bit cold. Wear something light that warms you, like a long-sleeved shirt with a sweater or fleece, topped with a jacket. ";
+		else if (temp < 10 && temp >= 0)
+			recommendation += "It is cold! Wear something warm, like sweater, topped with warm jacket and jeans. ";
+		else
+			recommendation += "It is freezing! Wear something really warm. Don't forget about the gloves, a scarf and a cap. ";
+		//Using also weather condition
+		if (weatherCondition.toLowerCase().contains("rainy")
+				|| weatherCondition.toLowerCase().contains("rain"))
+			recommendation += "It is also rainy, so don't forget about umbrella!";
+
+		return recommendation;
 	}
 
 	protected static int analyseWind(Double wind) {
