fc66729cb20e433b7b21fb834a5520bc64f75315
anna <2004gromyko@gmail.com>
2024-04-17 16:55:23 +0100

    tested and implemented clothes method in Recommendation java
    tested and implemented clothes method in Recommendation java class

--- XBOT/src/test/java/clothing/RecommendationTest.java
+++ XBOT/src/test/java/clothing/RecommendationTest.java
@@ -79,6 +79,17 @@ class RecommendationTest {
 		assertEquals(4, Recommendation.analyseWind(110.0));// Very strong wind
 	}
 
-	
+	@Test
+	public void clothesTest() {
+		// Test cases for different temperature, weather conditions, and wind speeds
+		assertEquals("It is hot! Wear something light, like shorts and t-shirt. Don't forget about headdress. ",
+				Recommendation.clothes(25.0, "Sunny", 5.0)); // Light wind, hot temperature
+		assertEquals("It is warm, but not hot. Wear something light but take the jacket.",
+				Recommendation.clothes(18.0, "Partly cloudy", 15.0)); // Moderate wind, warm temperature
+		assertEquals("It is cold! Wear something warm, like sweater, topped with warm jacket and jeans. ",
+				Recommendation.clothes(8.0, "Cloudy with showers", 30.0)); // Strong wind, cold temperature
+		assertEquals("IT IS STORM! Stay at home!",
+				Recommendation.clothes(8.0, "Cloudy with showers", 120.0));// Storm
+	}
 
 }
