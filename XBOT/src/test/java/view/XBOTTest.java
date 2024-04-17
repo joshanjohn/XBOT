f139c429ab4b0749f2888a8f540033912a064631
Joshanjohn <54502617+joshanjohn@users.noreply.github.com>
2024-04-17 17:09:49 +0100

    Created XBOT Constructor

---
+++ XBOT/src/test/java/view/XBOTTest.java
@@ -0,0 +1,36 @@
+package view;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.text.DateFormat.Field;
+import java.util.Calendar;
+
+import javax.swing.JTextArea;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class XBOTTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	
+
+}
